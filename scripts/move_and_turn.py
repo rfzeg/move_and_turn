c8adff7d022696abad536ba17ead6e05ebffcb3c
Roberto Zegers <rzegers@uc.cl>
2023-05-18 14:51:32 +0000

    Add log print to inform about node start

--- scripts/move_and_turn.py
+++ scripts/move_and_turn.py
@@ -41,6 +41,7 @@ def start_move_and_turn_callback(req):
 
 
 rospy.init_node('move_and_turn_node', anonymous=False)
+rospy.loginfo("Started move_and_turn node")
 pub_cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 move_distance = 0.5  # distance in meters to move straight
 turn_angle = 90.0  # angle in degrees to turn
